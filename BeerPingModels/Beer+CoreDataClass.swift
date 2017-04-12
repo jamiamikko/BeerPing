2362d1a08f9ab8fa09e4df1a15f37fb069b65ada
Mikko Jämiä <jamiamikko@Mikko-MacBook-Pro-5.local>
2017-04-12 10:00:46 +0300

    Create model classes for entities

---
+++ BeerPingModels/Beer+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Beer+CoreDataClass.swift
+//  BeerPing
+//
+//  Created by Mikko Jämiä on 12/04/2017.
+//  Copyright © 2017 BeerBluds. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Beer: NSManagedObject {
+
+}
---
+++ BeerPingModels/Beer+CoreDataProperties.swift
@@ -0,0 +1,46 @@
+//
+//  Beer+CoreDataProperties.swift
+//  BeerPing
+//
+//  Created by Mikko Jämiä on 12/04/2017.
+//  Copyright © 2017 BeerBluds. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension Beer {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Beer> {
+        return NSFetchRequest<Beer>(entityName: "Beer")
+    }
+
+    @NSManaged public var id: Int16
+    @NSManaged public var name: String?
+    @NSManaged public var brewer: String?
+    @NSManaged public var desc: String?
+    @NSManaged public var abv: Float
+    @NSManaged public var type: String?
+    @NSManaged public var price: String?
+    @NSManaged public var ibu: Int16
+    @NSManaged public var bars: NSSet?
+
+}
+
+// MARK: Generated accessors for bars
+extension Beer {
+
+    @objc(addBarsObject:)
+    @NSManaged public func addToBars(_ value: Bar)
+
+    @objc(removeBarsObject:)
+    @NSManaged public func removeFromBars(_ value: Bar)
+
+    @objc(addBars:)
+    @NSManaged public func addToBars(_ values: NSSet)
+
+    @objc(removeBars:)
+    @NSManaged public func removeFromBars(_ values: NSSet)
+
+}
