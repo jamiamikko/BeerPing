26ce8c32c46643faed3b0bb6f995dd6f3034f45a
Mikko Jämiä <jamiamikko@Mikko-MacBook-Pro-5.local>
2017-04-12 14:00:53 +0300

    Core data version

--- BeerPing/BarTableViewController.swift
+++ BeerPing/BarTableViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class BarTableViewController: UITableViewController {
     
@@ -15,13 +16,6 @@ class BarTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        bars = ["Roopen baari :D", "Mikon baari :D", "Oton baari :D"]
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
---
+++ BeerPingModels/CurrentVersion+CoreDataProperties.swift
@@ -0,0 +1,21 @@
+//
+//  CurrentVersion+CoreDataProperties.swift
+//  BeerPing
+//
+//  Created by Mikko Jämiä on 12/04/2017.
+//  Copyright © 2017 BeerBluds. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension CurrentVersion {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<CurrentVersion> {
+        return NSFetchRequest<CurrentVersion>(entityName: "CurrentVersion")
+    }
+
+    @NSManaged public var version: Int32
+
+}
